7b7f5e3f7e969564a16e32a560d72700fb391c63
ayman damoun <ayman.d@enchanted.tools>
2024-02-02 22:57:48 +0100

    clean
    Signed-off-by: ayman damoun <ayman.d@enchanted.tools>

--- build/abb_irb2600_interface/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/geekayman/irb2600_ws/src/abb_irb2600_interface
-# Build directory: /home/geekayman/irb2600_ws/build/abb_irb2600_interface
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/abb_irb2600_iterface/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/geekayman/irb2600_ws/src/abb_irb2600_iterface
-# Build directory: /home/geekayman/irb2600_ws/build/abb_irb2600_iterface
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
